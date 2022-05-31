30de9639726b1cfbc7df1ee876461bd9429c0ef0
ness <duranr@protonmail.com>
2022-05-30 20:28:30 -0400

    created Game Class

--- main.py
+++ main.py
@@ -3,22 +3,33 @@ import pygame
 from sys import exit
 from settings import *
 
-# Init
-pygame.init()
-screen = pygame.display.set_mode((screenWidth, screenHeight))
-clock = pygame.time.Clock()
+# Game Class
+# When code scales, it will be much easier to modify this class rather than a function
+class Game:
+    # Init
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(gameName)
+        
+        # Icon 
+        self.Icon = pygame.image.load(iconPath)
+        pygame.display.set_icon(self.Icon)
+        #######################################
+        
+        self.screen = pygame.display.set_mode((screenWidth, screenHeight))
+        self.clock = pygame.time.Clock()
+    
+    # Game Loop
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+            self.screen.fill(blackRGB)
+            pygame.display.update()
+            self.clock.tick(FPS)
 
-# Game Loop
-def gameLoop():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-        screen.fill('black')
-        pygame.display.update()
-        clock.tick(FPS)
-
-# Run Game
-gameLoop()
-
+# Create Game OBJ and call run
+game = Game()
+game.run()
--- settings.py
+++ settings.py
@@ -1,5 +1,10 @@
 # Constants
+gameName = "Test Game"
+iconPath = "data/img/testIcon.jpg"
 screenWidth = 1280
 screenHeight = 720
 FPS = 60
 tileSize = 64
+
+# Misc.
+blackRGB = (0,0,0)
